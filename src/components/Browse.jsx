96fd5974464d041bf945f24841682c592875c2c6
Dilip Woad <dilipwoad777@gmail.com>
2024-12-21 14:33:17 +0530

    component

---
+++ src/components/Browse.jsx
@@ -0,0 +1,9 @@
+const Browse=()=>{
+    return(
+        <div>
+            This is To Browse Netflix
+        </div>
+    )
+}
+
+export default Browse;
---
+++ src/components/Login.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header";
+
+const Login =()=>{
+    return(
+        <div>
+            <Header/>
+            <div>
+                <img src="https://assets.nflxext.com/ffe/siteui/vlv3/150c4b42-11f6-4576-a00f-c631308b1e43/web/IN-en-20241216-TRIFECTA-perspective_915a9055-68ad-4e81-b19a-442f1cd134dc_small.jpg" alt="background"/>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
