41f4fe9a24b522f4d4284fa61b12d8adb5c1c0e6
Dilip Woad <dilipwoad777@gmail.com>
2025-03-04 16:29:22 +0530

    Mobile view done,Laptop view started

--- src/components/ActorInformationComponents/ActorCredits.jsx
+++ src/components/ActorInformationComponents/ActorCredits.jsx
@@ -10,7 +10,7 @@ const ActorCredits = () => {
   return (
     <div className="bg-pink-400 mt-3 p-2">
       <div className="font-semibold text-2xl my-3">Known For</div>
-      <div className="flex flex-wrap justify-evenly">
+      <div className="flex flex-wrap justify-between  md:justify-center bg-lime-300">
         {movieCredits.cast.map((credit) => {
           if (!credit.poster_path) return;
           movieCount += 1;
@@ -21,9 +21,9 @@ const ActorCredits = () => {
                 poster={credit.poster_path}
                 movieId={credit.id}
                 css={
-                  "min-w-28 my-2 flex md:p-2 rounded-lg md:min-w-60 md:h-auto md:flex md:justify-center hover:cursor-default "
+                  "min-w-24 flex m-2 rounded-lg md:p-2  md:min-w-60 md:h-auto md:flex md:justify-center hover:cursor-default "
                 }
-                postercss={"min-w-28 md:h-56 rounded-md hover:cursor-pointer"}
+                postercss={"min-w-[100px] md:min-w-28 md:h-56 rounded-md hover:cursor-pointer"}
               />
             );
           }
@@ -32,10 +32,10 @@ const ActorCredits = () => {
         { movieCredits.cast.length >= count &&
             <div
             onClick={() => {setViewAll(!viewAll), setcount(!viewAll? movieCredits.cast.length : 11)}}
-            className=" flex items-center w-28 text-center justify-center"
+            className=" flex m-2 w-[99px] items-center text-center justify-center md:w-40"
           >
-            <div className="bg-red-300 h-16 rounded-lg flex justify-center items-center  w-full text-xl font-semibold text-nowrap">
-            {viewAll ? "View Less" : "View ALL"}
+            <div className="bg-red-300 w-full h-16 rounded-lg flex justify-center items-center text-lg md:text-xl font-semibold text-nowrap">
+            {viewAll ? "View Less" : "View All"}
             </div>
           </div>
         }
--- src/components/ActorInformationComponents/ActorProfile.jsx
+++ src/components/ActorInformationComponents/ActorProfile.jsx
@@ -4,37 +4,43 @@ import { ACTOR_EMPTY_ICON, MOVIE_IMG } from "../../utils/constant";
 import { AgeCalculator } from "../../utils/AgeCalculator";
 
 const ActorProfile = () => {
-    const actorDetails = useSelector((store)=>store?.actor?.details);
+  const actorDetails = useSelector((store) => store?.actor?.details);
   const [readMore, setReadMore] = useState(false);
-  const overview = actorDetails?.biography
-  const age = AgeCalculator(actorDetails?.birthday)
+  const overview = actorDetails?.biography;
+  const age = AgeCalculator(actorDetails?.birthday);
   return (
     <>
       <div className=" bg-lime-300 flex justify-center items-center flex-col">
         <img
-          className="bg-purple-400 h-60 w-auto mt-5 mb-3 "
-          src={actorDetails.profile_path ? MOVIE_IMG+actorDetails?.profile_path : ACTOR_EMPTY_ICON}
+          className="bg-purple-400 h-64 w-auto mt-5 mb-3 rounded-lg shadow-xl "
+          src={
+            actorDetails.profile_path
+              ? MOVIE_IMG + actorDetails?.profile_path
+              : ACTOR_EMPTY_ICON
+          }
           alt="actor-image"
         />
-        <div className="text-center w-72 ">
+        <div className="text-center w-72 mb-4 bg-sky-300">
           <h1 className="text-4xl font-bold">{actorDetails?.name}</h1>
         </div>
       </div>
-      <div className="bg-yellow-200 my-3 p-2">
-        <h1>Age : {age}</h1>
-        <h1>Birth Date : {actorDetails?.birthday}</h1>
-        <h1>Born At : {actorDetails?.place_of_birth}</h1>
-      </div>
-      <div className="bg-purple-400 p-2">
-        <p className="font-semibold text-lg text-justify p-2">
-          Biography : {readMore ? overview : overview.substring(0, 300)}{" "}
-          <span
-            onClick={() => setReadMore(!readMore)}
-            className=" text-white font-bold  opacity-50 rounded-md hover:opacity-100 hover:cursor-pointer"
-          >
-            {readMore ? "Read Less" : "Read More..."}
-          </span>
-        </p>
+      <div className="bg-orange-300 md:flex">
+        <div className="bg-yellow-200 my-3 p-2 md:flex-2">
+          <h1>Age : {age}</h1>
+          <h1>Birth Date : {actorDetails?.birthday}</h1>
+          <h1>Born At : {actorDetails?.place_of_birth}</h1>
+        </div>
+        <div className="bg-purple-400 p-2 md:flex-1">
+          <p className="font-semibold text-lg text-justify p-2">
+            Biography : {readMore ? overview : overview.substring(0, 300)}{" "}
+            <span
+              onClick={() => setReadMore(!readMore)}
+              className=" text-white font-bold  opacity-50 rounded-md hover:opacity-100 hover:cursor-pointer"
+            >
+              {readMore ? "Read Less" : "Read More..."}
+            </span>
+          </p>
+        </div>
       </div>
     </>
   );
